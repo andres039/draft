cf381e8df608edb1edb51f6a05fe10dc9326a614
Andres <andres039039@hotmail.com>
2023-03-10 10:39:30 -0500

    fix layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import Layout from "../components/Layout";
+import Layout from "../components/layout";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
